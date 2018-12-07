a79b63ed3c2ca9bf48635b2ddf8be95f47bcd3d5
Duncan Dam <duncandam@dhcp-10-101-120-20.wireless.northwestern.private>
2018-12-07 15:18:15 -0600

    JS html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,60 @@
-const peopleArr = require("../data/friends");
+let friendsArr = require("../data/friends");
 
 //export api route
 module.exports = function (app) {
     // return all friends found in peopleArr via API
     app.get("/api/friends", function (req, res) {
-        return res.json(peopleArr);
+        return res.json(friendsArr);
     });
+
+    app.post("/api/friends", function (req, res) {
+        let newUser = req.body;
+
+        console.log(newUser)
+
+
+        //parseINt all the scores since input scores are string
+        for (var i = 0; i < newUser.length; i++) {
+            newUser.scores[i] = parseInt(newUser.scores[i]);
+        }
+        // create variable to hold index for potential best friend and differences in score
+        let totalDiffArr = [];
+        let index;
+        //loop through all friends in the friendArr
+        for (i = 0; i < friendsArr.length; i++) {
+            let diffArr = []
+            //loop through all score of each user or friend
+            for (j = 0; j < friendsArr[i].scores.length; j++) {
+                //create variable to store difference between new user and any of the current friend in the array
+                quesDiff = Math.abs(newUser.scores[j] - friendsArr[i].scores[j]);
+                diffArr.push(quesDiff);
+            }
+            // console.log(difference);
+            // calculate totalDiff
+            var totalDiff = diffArr.reduce(add, 0);
+            function add(a, b) {
+                return a + b
+            }
+            add(totalDiff)
+            console.log(totalDiff);
+
+            //push totalDiff from each friend to the total Diff Array
+            totalDiffArr.push(totalDiff);
+            console.log(totalDiffArr);
+            
+            //identify the lowest difference among all totalDiif
+            let minDiff = Math.min.apply(null, totalDiffArr);
+            console.log(minDiff)
+            index = totalDiffArr.indexOf(minDiff)
+            console.log(index)
+        }
+
+        // push new user into the new freinds Array
+        friendsArr.push(newUser);
+        res.json(friendsArr)
+        console.log("your new best friend is:" + friendsArr[index].name);
+        // res.json(friendsArr);
+        // res.json(friendsArr[index])
+    })
 
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,11 +5,11 @@ module.exports = function (app) {
     //set up html route
     //home page
     app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "home.html"));
+        res.sendFile(path.join(__dirname, "/../public/home.html" ));
     });
     //survey
     app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "survey.html"));
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
     });
 
 }
